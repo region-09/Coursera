46ef7e2d373a36b439099b01cf65845e92ff77bd
region-09 <ruslanovich.ik@gmail.com>
2022-01-16 15:48:52 +0100

    shiftkey showing coordinates

---
+++ Javascipt/practice2/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded",
+    function (event) {
+        function sayHello (event) {
+        this.textContent = "said it"
+        var name = document.getElementById("name").value;
+        var message = "<h2>Hello! " + name + "</h2>";
+        document.getElementById("content").innerHTML = message;
+
+        if (name === "student") {
+            var title = document.querySelector("#title").textContent;
+            title += " & Lovin it!";
+            document.querySelector("#title").textContent = title;
+        }
+        }
+        /******  Unobstrusive event binding   (you can delete onclick on index.html)******/
+        document.querySelector("button").addEventListener("click", sayHello);
+});
+// document.querySelector("button").onclick = sayHello;
